480cb0d577abb2aeb6e1567349ead436d371a442
Deric Walintukan <deric@loopapps.com>
2020-10-05 10:26:08 -0700

    Add first view test

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -1,7 +1,18 @@
+/**
+ * 1. Run the Calculator: yarn start / npm start
+ * 2. Run the Cypress Test Runner: yarn cypress / npm run cypress
+ * 3. Write more tests for the calculator
+ * 4. Run the tests in the Cypress window
+ */
+
 context('Calculator', () => {
-  describe('simple test', () => {
-    it('checks true', () => {
-      expect(true).to.equal(true)
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  describe('views', () => {
+    it('checks the root component is visible', () => {
+      cy.get('.component-app').should('be.visible')
     })
   })
 })
